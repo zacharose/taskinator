0eb0ca41dd44cf86cf309b471f3862ed77b33ee7
Zach rose <zachrose@ymail.com>
2021-07-04 23:03:57 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,24 @@ var taskToDoEl = document.querySelector("#task-to-do");
 var createTaskHandler = function(event) {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value
+    
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //give it a class name
+    taskInfoEl.className = "task-info";
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
+    // listItemEl.textContent = taskNameInput;
     taskToDoEl.appendChild(listItemEl);
 }
 
